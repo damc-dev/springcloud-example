5bbb7994bd100d9ec754985536e7ff66847839c9
David McElligott <EMAIL>
2018-03-19 16:45:17 -0500

    Trying to fix hystrix timeout on first request to /recommendation service... still needs work

--- book-recommendation-service/src/main/groovy/com/dmcelligott/books/recommend/BookRecommendationApplication.groovy
+++ book-recommendation-service/src/main/groovy/com/dmcelligott/books/recommend/BookRecommendationApplication.groovy
@@ -1,5 +1,7 @@
 package com.dmcelligott.books.recommend
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -14,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController
 @EnableDiscoveryClient
 class BookRecommendationApplication {
 
+    Logger log = LoggerFactory.getLogger(this.class)
+
     @RequestMapping("/recommended")
     @ResponseBody
     String recommendedBooks() {
+        log.info("Returning recommended books")
         return "Spring in Action (Manning), Cloud Native Java (O'Reilly), Learning Spring Boot (Packt)"
     }
 
